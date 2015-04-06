7cf0cde7e28e6a1c2a89bec1d5409399a91f3933
Z.Li <lizhi.magic@gmail.com>
2015-04-06 11:23:53 -0400

    update ground communication layer

--- prototype/dchat.cpp
+++ prototype/dchat.cpp
@@ -3,9 +3,10 @@
 #include <vector>
 #include <sstream>
 #include <thread>
-#include "User.h"
-#include "ChatNode.h"
+// #include "User.h"
+// #include "ChatNode.h"
 #include "util.h"
+#include <string.h>
 
 extern "C"
 {
@@ -14,18 +15,39 @@ extern "C"
 
 using namespace std;
 
+// for test purpose, type msgs are all sent to creator
+static char test_port[10];
+
 void receive()
 {
 	// call stub_receive
-	stub_receive();
+	while(1)
+	{
+		stub_receive();
+	}
 }
 
 void type()
 {
-	// call ChatNode to parse down msg
-	
-	// call stub_send
-	stub_send(string Tip, string Tport, string msg);
+	while(1)
+	{
+		// call ChatNode to parse down msg
+		char msg[512];
+		char ip[20], port_[20];
+		std::cin.getline (msg, 512);
+		if (strlen(msg) == 0)
+		{
+			// won't send blank message
+			std::cout << "Hint: blank messages won't be sent.\n";
+
+		} else {
+			strcpy(ip, "127.0.0.1");
+			strcpy(port_, test_port);
+			// strcpy(port_,"20000");
+			// call stub_send
+			stub_send(ip, port_, msg);
+		}
+	}
 }
 
 void check()
@@ -35,30 +57,60 @@ void check()
 
 int main(int argc, char** argv)
 {
-	ChatNode node;
+	// ChatNode node;
 	if(argc == 2)
 	{
 		string name(argv[1]);
-		string ip = "127.0.0.1";
-		int port = randomPort();
-		User user(ip,name,port);
-		node.controlInitChat(user);	
+		std::cout << "Create Chat, name: "<< name << endl;
+		// create
+		string handle = string(stub_create());
+		if ( handle.compare("ERROR") == 0 ) 
+		{
+			// ERROR
+			std::cout << "Met error, could not create chat.\n";
+			exit(1);
+		} else {
+			size_t pos = handle.find(":");
+			if (pos == string::npos)
+			{
+				cout << "please enter address as 0.0.0.0:1234" << endl;
+				exit(1);
+			} 
+
+			char *ip = (char *) handle.substr(0, pos).c_str();		
+			char *port = (char *) handle.substr(pos+1, -1).c_str();
+			strcpy(test_port, port);
+		}
+		
 	}else if(argc == 3)
 	{
 		string name(argv[1]);
-		string address(argv[2]);
-		vector<string> strings;
-		istringstream f(address);
-		string ownIP = "127.0.0.1";
-		string ip;
-		string portStr; 
-    	getline(f, ip, ':');
-    	getline(f, portStr, ':');
-		int port = atoi(portStr.c_str()); 
-		int ownPort = randomPort();
-		User user(ownIP, name, ownPort);
-        node.setCurrentUser(user);
-		node.join(user, ip, port);
+		string addr(argv[2]);
+		// string ip, port;
+		size_t pos = addr.find(":");
+		if (pos == string::npos)
+		{
+			cout << "please enter address as 0.0.0.0:1234" << endl;
+			exit(1);
+		} 
+		
+		std::cout << name << " attempts to join chat at "<< addr << endl;
+		char *ip = (char *) addr.substr(0, pos).c_str();		
+		char *port = (char *) addr.substr(pos+1, -1).c_str();
+		
+		strcpy(test_port, port);
+		
+		string handle = string(stub_connect(ip, port));
+		if ( handle.compare("ERROR") == 0 ) 
+		{
+			// ERROR
+			std::cout << "Met error, could not join chat.\n";
+			exit(1);
+		}
+		
+	} else {
+		std::cout << "[./dchat Bob] or [./dchat Alice 192.168.1.101:3000]\n";
+		exit(1);
 	}
 	
 	
--- prototype/multicast.c
+++ prototype/multicast.c
@@ -1,8 +1,6 @@
 #include "multicast.h"
 // local ip, port for dchat
-#define MAXBUFLEN 9999;
-static int port;
-static char* ip;
+#define MAXBUFLEN 9999
 static int sockfd; //used for listener
 
 void *get_in_addr(struct sockaddr *sa)
@@ -16,12 +14,31 @@ void *get_in_addr(struct sockaddr *sa)
 
 char* getlocalinfo()
 {
-	char* msg, port_s;
+	int port;
+	char msg[30];
+	char port_[10];
+	char ip[20];
+	
 	// get local ip, port info; ip:port	
-	itoa(port,port_s,10);
+	// could only get 0.0.0.0 from listening socket
+	// could get real ip from send socket
+	struct sockaddr_in localAddress;
+	socklen_t addressLength = sizeof(localAddress);
+	getsockname(sockfd, (struct sockaddr*)&localAddress, &addressLength);
+	
+	port = (int) ntohs(localAddress.sin_port);
+	strcpy(ip, inet_ntoa(localAddress.sin_addr));
+	printf("local address: %s\n", ip);
+	printf("local port: %d\n", port);
+	
+	sprintf(port_, "%d", port);	
 	strcat(msg,ip);
 	strcat(msg,":");
-	strcat(msg,port_s);
+	strcat(msg,port_);
+	
+	printf("stub: finish binding. ip:port -> %s:%d\n",ip,port);
+    printf("stub: waiting to recvfrom...\n");
+	
 	return msg;
 }
 
@@ -29,21 +46,21 @@ char* getlocalinfo()
 char* stub_create()
 {
     struct addrinfo hints, *servinfo, *p;
-    int rv;
-	// set timeout val
-	struct timeval tv;
-	tv.tv_sec = 5;  /* 5 Secs Timeout */
-	tv.tv_usec = 0;  // Not init'ing this can cause strange errors
+    int rv, port;
+	char port_[20];
 	
 	// randomly assign a port number
 	port = randomPort();
+	// port = 20000;
+	sprintf(port_,"%d", port);
+	printf("stub: PORT specified: %d\n", port);
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
     hints.ai_socktype = SOCK_DGRAM; // use UDP
     hints.ai_flags = AI_PASSIVE; // use my IP
 
-    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(NULL, port_, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return "ERROR";
     }
@@ -55,12 +72,6 @@ char* stub_create()
             perror("stub: socket");
             continue;
         }
-		
-		// set timeout
-		if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval))== -1) {
-		            perror("setsockopt");
-		            return "ERROR";
-		        }
 
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             close(sockfd);
@@ -77,86 +88,22 @@ char* stub_create()
     }
 
     freeaddrinfo(servinfo); // done with servinfo
-	
-	// update current ip
-	struct sockaddr_in localAddress;
-	socklen_t addressLength = sizeof(localAddress);
-	getsockname(sockfd, (struct sockaddr*)&localAddress, &addressLength);
-	printf("local address: %s\n", inet_ntoa(localAddress.sin_addr));
-	printf("local port: %d\n", (int) ntohs(localAddress.sin_port));
-	
-	strcpy(ip, inet_ntoa(localAddress.sin_addr));
-	printf("stub: finish binding. ip:port -> %s:%d\n",ip,port);
-    printf("stub: waiting to recvfrom...\n");
 	return getlocalinfo();
 			
 }
 
 char* stub_connect(char* Tip, char* Tport)
 {
-	// used for join, sendto
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    int numbytes;
-	// set timeout val
-	struct timeval tv;
-	tv.tv_sec = 5;  /* 5 Secs Timeout */
-	tv.tv_usec = 0;  // Not init'ing this can cause strange errors
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
+	char msg[20];	
+	strcpy(msg, "connect");
+		
+	if ( strcmp(stub_send(Tip, Tport, msg), "ERROR") == 0 ) return "ERROR";
 
-    if ((rv = getaddrinfo(Tip, Tport, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return "ERROR";
-    }
+	strcpy(msg, stub_create());
+	if ( strcmp(msg, "ERROR") == 0 ) { return "ERROR"; 	}
+	else { return msg; }
 
-    // loop through all the results and make a socket
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("stub: socket error");
-            continue;
-        }
-		// set timeout
-		if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval))== -1) {
-		            perror("setsockopt");
-		            return "ERROR";
-		        }
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "stub: failed to get addr\n");
-        return "ERROR";
-    }
-    freeaddrinfo(servinfo);
-	
-	// send connect msg
-	char *str;
-	strcpy(str,"connect");
-    if ((numbytes = sendto(sockfd, str, strlen(str), 0,
-             p->ai_addr, p->ai_addrlen)) == -1) {
-        perror("stub: connect");
-        return "ERROR";
-    }
-	printf("stub: sent %d bytes to %s\n", numbytes, Tip);
-
-	// update IP, port
-	struct sockaddr_in localAddress;
-	socklen_t addressLength = sizeof(localAddress);
-	getsockname(sockfd, (struct sockaddr*)&localAddress, &addressLength);
-	printf("local address: %s\n", inet_ntoa(localAddress.sin_addr));
-	printf("local port: %d\n", (int) ntohs(localAddress.sin_port));
-	
-	strcpy(ip, inet_ntoa(localAddress.sin_addr));
-	port = (int) ntohs(localAddress.sin_port);
-	
-	printf("stub: finish binding. ip:port -> %s:%d\n",ip,port);
-    printf("stub: waiting to recvfrom...\n");	
-	
-	return getlocalinfo();
 }
 
 char* stub_receive()
@@ -166,27 +113,42 @@ char* stub_receive()
     char buf[MAXBUFLEN];
     socklen_t addr_len;
     char s[INET6_ADDRSTRLEN];
-	char* Sip;
+	char Sip[30];
 
     addr_len = sizeof their_addr;
 	
     if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
         (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-        perror("recvfrom");
+        perror("recvfrom error");
         return "ERROR";
     }
 	
 	// store source ip
-	Sip = inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+	strcpy(Sip, inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s));
     printf("stub: got packet from %s\n", Sip);
 	
-	// printf("from port: %hu\n", *get_in_port((struct sockaddr *)&their_addr));
     printf("stub: packet is %d bytes long\n", numbytes);
     buf[numbytes] = '\0';
     printf("stub: packet contains \"%s\"\n", buf);
-
+		
+	char str[20];
+	strcpy(str, "OK:3");
+	if ( buf[0] == '0') {
+		// connect received, send ack back
+		if ((numbytes = sendto(sockfd, str, strlen(str), 0, 
+			(struct sockaddr *)&their_addr, addr_len)) == -1) {
+	        perror("stub: sendto");
+	        return "ERROR";
+	    }
+		
+	    printf("stub: send %d bytes to %s\n	msg contains: %s\n\n", numbytes, inet_ntop(their_addr.ss_family,
+	            get_in_addr((struct sockaddr *)&their_addr),
+	            s, sizeof s), str);
+	}
+	
 	// parse string received
-	parsePara(&buf);
+	// parsePara(&buf);
+	return "SUCCESS";
 }
 
 char* stub_send(char* Tip, char* Tport, char* msg)
@@ -196,11 +158,18 @@ char* stub_send(char* Tip, char* Tport, char* msg)
     int rv;
     int numbytes; 
 	
-	// // receive msg from server, for ack
-	//     struct sockaddr_storage their_addr;
-	//     char buf[MAXBUFLEN];
-	//     socklen_t addr_len;
-	//     char s[INET6_ADDRSTRLEN];
+	// receive msg from server, for ack
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+	
+	// customize request sent
+	char fullmsg[512];
+	// sometimes char* mess up with address in memory
+	// printf("Target IP:%s\n", Tip);
+	// printf("Target PORT:%s\n", Tport); // check if Tport is shifted to "msg"
+	
 	// set timeout val
 	struct timeval tv;
 	tv.tv_sec = 5;  /* 5 Secs Timeout */
@@ -237,13 +206,40 @@ char* stub_send(char* Tip, char* Tport, char* msg)
     }
 
 	// customize request sent
-	printf("stub: msg prepared to send: %s\n",msg);
-    if ((numbytes = sendto(sockfd_w, msg, strlen(msg), 0,
+	strcpy(fullmsg,"");
+	strcat(fullmsg, "00000"); // add prefix
+	strcat(fullmsg, msg);
+	// strcpy(fullmsg, "0ABCDEF"); // test
+	printf("stub: msg prepared to send: %s\n",fullmsg);
+    if ((numbytes = sendto(sockfd_w, fullmsg, strlen(fullmsg), 0,
              p->ai_addr, p->ai_addrlen)) == -1) {
         perror("stub: sendto");
         return "ERROR";
     }
 	printf("stub: sent %d bytes to %s\n", numbytes, Tip);
+	
+	// receive return msg from server
+	// ack timeout 5s
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd_w, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("stub: recvfrom");
+        return "ERROR";
+    }
+	
+    printf("stub: received packet from %s\n",
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s));
+
+    printf("stub: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("stub: packet contains: %s\n", buf);
+
+
+    close(sockfd_w);
+	
+	return "SUCCESS";
 	
 }
 
