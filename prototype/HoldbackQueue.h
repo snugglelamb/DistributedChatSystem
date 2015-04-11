ecf50f5c00c65d9e04a0bb53b83aa98ec5842a27
siyuqtt <siqiu92@gmail.com>
2015-04-11 19:46:04 -0400

    add holdbackqueue

---
+++ prototype/HoldbackQueue.h
@@ -0,0 +1,22 @@
+#ifndef HOLDBACKQUEUE_H
+#define HOLDBACKQUEUE_H
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class HoldbackQueue
+{
+public:
+	vector<int> queue;
+
+	void enqueue(int val);
+	void dequeue();
+	int peek();
+
+	HoldbackQueue();
+
+};
+
+#endif
