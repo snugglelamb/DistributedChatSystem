26abccd3875f67fccb0112517e69806251d83bb1
siyuqtt <siqiu92@gmail.com>
2015-04-12 20:04:21 -0400

    i

--- prototype/Queue.h
+++ prototype/Queue.h
@@ -20,50 +20,9 @@
 //using namespace boost;
 using namespace std;
 
-using namespace std;
-
 class Queue
 {
  public:
-
-  string pop()
-  {
-    unique_lock<mutex> mlock(mutex_);
-    while (queue_.empty())
-    {
-      cond_.wait(mlock);
-    }
-    auto val = queue_.front();
-    queue_.pop_front();
-    return val;
-  }
-
-  void pop(string& item)
-  {
-    unique_lock<mutex> mlock(mutex_);
-    while (queue_.empty())
-    {
-      cond_.wait(mlock);
-    }
-    item = queue_.front();
-    queue_.pop_front();
-  }
-  void showqueue(){
-	  unique_lock<mutex> mlock(mutex_);
-	  //mutex_.lock();
-	  for(auto it : queue_){
-		  cout<< it<<" ";
-	  }
-	  cout<<endl;
-	  mlock.unlock();
-  }
-  void push(const string& item)
-  {
-    unique_lock<mutex> mlock(mutex_);
-    queue_.push_back(item);
-    mlock.unlock();
-    cond_.notify_one();
-  }
 
   string pop();
 
