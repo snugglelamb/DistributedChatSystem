9698bdc71de86e4c7343eab4792a95423209fdbc
Z.Li <lizhi.magic@gmail.com>
2015-04-14 13:39:23 -0400

    add fault tolerance

---
+++ talker_listener/multicast.h
@@ -0,0 +1,33 @@
+#ifndef MULTICAST_H
+#define MULTICAST_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+extern "C"
+{
+	#include <sys/socket.h>
+	#include <sys/types.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <stdio.h>
+	#include <string.h>
+	#include <stdlib.h>
+	#include <unistd.h>
+	#include <errno.h>
+	#include <netdb.h>
+	#include "util.h"
+	#include <ifaddrs.h>
+	#include <net/if.h>
+}
+// #include "parser.h"
+extern int sendQ_num;
+
+std::string stub_connect(const char* Tip, const char* Tport);
+std::string stub_receive();
+std::string stub_send(const char* Tip, const char* Tport, const char* msg, int request);
+std::string stub_create();
+std::string getlocalinfo();
+void* get_in_addr(struct sockaddr *sa);
+
+#endif
---
+++ talker_listener/util.h
@@ -0,0 +1,5 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+int randomPort();
+#endif
